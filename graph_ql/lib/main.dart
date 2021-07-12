43d8d24b56c2173fe1da53360b06b2347b4a8c4a
nextishwar <nextishwar@gmail.com>
2021-07-12 14:04:55 +0530

    getting details

--- graph_ql/lib/main.dart
+++ graph_ql/lib/main.dart
@@ -11,7 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final httpLink = HttpLink("https://countries.trevorblades.com/");
     final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
-        GraphQLClient(link: httpLink, cache: GraphQLCache()));
+      GraphQLClient(
+        link: httpLink,
+        cache: GraphQLCache(),
+      ),
+    );
     return GraphQLProvider(
       child: HomePage(),
       client: client,
@@ -25,32 +29,114 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController c1 = TextEditingController();
+  TextEditingController c2 = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("GraphQl"),
-        ),
-        body: Query(
-          builder: (result, {fetchMore, refetch}) {
-            if (result.data == null) {
-              return Text("No data found");
-            }
-            return ListView.builder(
-              itemBuilder: (BuildContext context, index) => Card(
-                child: Row(
-                  children: [
-                    Text("${result.data?['continents'][index]['name']}"),
+      appBar: AppBar(
+        title: Text("GraphQl"),
+      ),
+      extendBody: true,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Query(
+                builder: (result, {fetchMore, refetch}) {
+                  if (result.data == null) {
+                    return Text("No data found");
+                  }
+                  if (result.isLoading) {
+                    return SizedBox(
+                        height: 20,
+                        width: 20,
+                        child: CircularProgressIndicator());
+                  }
+                  return SizedBox(
+                    height: 200,
+                    child: ListView.builder(
+                      itemBuilder: (BuildContext context, index) => Card(
+                        child: Row(
+                          children: [
+                             Text("${result.data?['continents'][index]['name']}"),
                     SizedBox(width: 20,),
                     Text("${result.data?['continents'][index]['countries'][index]['name']}"),
-                  ],
-                ),
+                          ],
+                        ),
+                      ),
+                      itemCount: result.data?['continents'].length,
+                    ),
+                  );
+                },
+                options: QueryOptions(document: gql(TodoFetch.fetchAll)),
               ),
-              itemCount: result.data?['continents'].length,
-            );
-          },
-          options: QueryOptions(document: gql(TodoFetch.fetchAll)),
-        ));
+              SizedBox(
+                height: 40,
+              ),
+              // Mutation(
+              //   options: MutationOptions(
+              //     document: gql(TodoFetch.addData),
+              //   ),
+              //   builder: (runMutation, result) => Column(
+              //     children: [
+              //       SizedBox(
+              //         height: 50,
+              //         width: 200,
+              //         child: TextFormField(
+              //           controller: c1,
+              //           decoration: InputDecoration(
+              //             hintText: "Name",
+              //             border: OutlineInputBorder(
+              //               borderRadius: BorderRadius.circular(8),
+              //             ),
+              //           ),
+              //         ),
+              //       ),
+              //       SizedBox(
+              //         height: 20,
+              //       ),
+              //       SizedBox(
+              //         height: 50,
+              //         width: 200,
+              //         child: TextFormField(
+              //           controller: c2,
+              //           decoration: InputDecoration(
+              //             hintText: "Code",
+              //             border: OutlineInputBorder(
+              //               borderRadius: BorderRadius.circular(8),
+              //             ),
+              //           ),
+              //         ),
+              //       ),
+              //       SizedBox(
+              //         height: 20,
+              //       ),
+              //       ElevatedButton(
+              //         onPressed: () {
+              //           runMutation(<String, dynamic>{
+              //             "Name":c1.text,
+              //             "Code":c2.text,
+              //           });
+              //         },
+              //         child: Text("Submit"),
+              //       )
+              //     ],
+              //   ),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
 
