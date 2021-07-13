8778bc6b3ad9375899b957988ccbcd38a89d3cda
nextishwar <nextishwar@gmail.com>
2021-07-13 09:50:46 +0530

    query, mutation and subscription with hasura server

--- graph_ql/lib/config/graphql_config.dart
+++ graph_ql/lib/config/graphql_config.dart
@@ -1,21 +1,53 @@
-// import "package:flutter/material.dart";
-// import "package:graphql_flutter/graphql_flutter.dart";
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-// class GraphQLConfiguration {
-//   static HttpLink httpLink = HttpLink("https://examplegraphql.herokuapp.com/graphql",
-//   );
+class Config {
+  static final HttpLink httpLink =
+      HttpLink('https://pet-drake-49.hasura.app/v1/graphql', defaultHeaders: {
+    'x-hasura-admin-secret':
+        "U8bcmJUM441Zh0n8OaqQrTa3fEktZa4rVM33gdA9A4sSdIQdKDx0eBaUsQf7AjW1",
+    "content-type": "application/json",
+  });
 
-//   ValueNotifier<GraphQLClient> client = ValueNotifier(
-//     GraphQLClient(
-//       link: httpLink,
-//       cache: GraphQLCache(dataIdFromObject: typenameDataIdFromObject),
-//     ),
-//   );
-
-//   GraphQLClient clientToQuery() {
-//     return GraphQLClient(
-//       cache: GraphQLCache(dataIdFromObject: typenameDataIdFromObject),
-//       link: httpLink,
-//     );
-//   }
-// }
+  static final WebSocketLink socketLink = WebSocketLink(
+    'wss://pet-drake-49.hasura.app/v1/graphql',
+    
+    config: SocketClientConfig(
+      initialPayload: () {
+        return {
+          'headers': {
+            'x-hasura-admin-secret':
+                "U8bcmJUM441Zh0n8OaqQrTa3fEktZa4rVM33gdA9A4sSdIQdKDx0eBaUsQf7AjW1"
+          },
+        };
+      },
+    ),
+    
+  );
+  // static String _token =
+  //     "U8bcmJUM441Zh0n8OaqQrTa3fEktZa4rVM33gdA9A4sSdIQdKDx0eBaUsQf7AjW1";
+  //static final AuthLink authLink = AuthLink(getToken: () => _token);
+  // static final WebSocketLink websocketLink = WebSocketLink(
+  //   'wss://hasura.io/learn/graphql',
+  //   config: SocketClientConfig(
+  //     autoReconnect: true,
+  //     inactivityTimeout: Duration(seconds: 30),
+  //     initialPayload: () async {
+  //       return {
+  //         'headers': {'x-hasura-admin-secret': _token},
+  //       };
+  //     },
+  //   ),
+  // );
+  // static final Link link = authLink.concat(httpLink).concat(websocketLink);
+  static ValueNotifier<GraphQLClient> initailizeClient(String token) {
+    //  _token = token;
+    ValueNotifier<GraphQLClient> client = ValueNotifier(
+      GraphQLClient(
+        cache: GraphQLCache(),
+        link: socketLink,
+      ),
+    );
+    return client;
+  }
+}
--- graph_ql/lib/main.dart
+++ graph_ql/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graph_ql/config/graphql_config.dart';
 import 'package:graphql/client.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
@@ -7,18 +8,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static String _token =
+      "U8bcmJUM441Zh0n8OaqQrTa3fEktZa4rVM33gdA9A4sSdIQdKDx0eBaUsQf7AjW1";
+  //  static final AuthLink authLink = AuthLink(getToken: () => _token);
+
   @override
   Widget build(BuildContext context) {
-    final httpLink = HttpLink("https://countries.trevorblades.com/");
-    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
-      GraphQLClient(
-        link: httpLink,
-        cache: GraphQLCache(),
-      ),
-    );
+    // Config.initailizeClient(_token);
+    //final httpLink = HttpLink("https://pet-drake-49.hasura.app/v1/graphql");
+
     return GraphQLProvider(
       child: HomePage(),
-      client: client,
+      client: Config.initailizeClient(_token),
     );
   }
 }
@@ -50,89 +51,97 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Query(
+              Subscription(
                 builder: (result, {fetchMore, refetch}) {
-                  
+                  print(result.toString());
                   if (result.isLoading) {
                     return SizedBox(
                         height: 20,
                         width: 20,
                         child: CircularProgressIndicator());
-                  }
-                  else if (result.data == null) {
+                  } else if (result.data == null) {
                     return Text("No data found");
                   }
+                  // print(result.toString());
                   return SizedBox(
                     height: 200,
                     child: ListView.builder(
                       itemBuilder: (BuildContext context, index) => Card(
                         child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                             Text("${result.data?['continents'][index]['name']}"),
-                    SizedBox(width: 20,),
-                    Text("${result.data?['continents'][index]['countries'][index]['name']}"),
+                            Text("${result.data?['Countries'][index]['Name']}"),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text("${result.data?['Countries'][index]['Code']}"),
                           ],
                         ),
                       ),
-                      itemCount: result.data?['continents'].length,
+                      itemCount: result.data?['Countries']?.length,
                     ),
                   );
                 },
-                options: QueryOptions(document: gql(TodoFetch.fetchAll)),
+                onSubscriptionResult: (subscriptionResult, client) {
+                  print(subscriptionResult.toString());
+                },
+                options: SubscriptionOptions(document: gql(subs)),
               ),
               SizedBox(
                 height: 40,
               ),
-              // Mutation(
-              //   options: MutationOptions(
-              //     document: gql(TodoFetch.addData),
-              //   ),
-              //   builder: (runMutation, result) => Column(
-              //     children: [
-              //       SizedBox(
-              //         height: 50,
-              //         width: 200,
-              //         child: TextFormField(
-              //           controller: c1,
-              //           decoration: InputDecoration(
-              //             hintText: "Name",
-              //             border: OutlineInputBorder(
-              //               borderRadius: BorderRadius.circular(8),
-              //             ),
-              //           ),
-              //         ),
-              //       ),
-              //       SizedBox(
-              //         height: 20,
-              //       ),
-              //       SizedBox(
-              //         height: 50,
-              //         width: 200,
-              //         child: TextFormField(
-              //           controller: c2,
-              //           decoration: InputDecoration(
-              //             hintText: "Code",
-              //             border: OutlineInputBorder(
-              //               borderRadius: BorderRadius.circular(8),
-              //             ),
-              //           ),
-              //         ),
-              //       ),
-              //       SizedBox(
-              //         height: 20,
-              //       ),
-              //       ElevatedButton(
-              //         onPressed: () {
-              //           runMutation(<String, dynamic>{
-              //             "Name":c1.text,
-              //             "Code":c2.text,
-              //           });
-              //         },
-              //         child: Text("Submit"),
-              //       )
-              //     ],
-              //   ),
-              // ),
+              Mutation(
+                options: MutationOptions(
+                  document: gql(TodoFetch.addData),
+                ),
+                builder: (runMutation, result) => Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 200,
+                      child: TextFormField(
+                        controller: c1,
+                        decoration: InputDecoration(
+                          hintText: "Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: 200,
+                      child: TextFormField(
+                        controller: c2,
+                        decoration: InputDecoration(
+                          hintText: "Code",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        runMutation(<String, dynamic>{
+                          "Name": c1.text,
+                          "Code": c2.text,
+                        });
+                      },
+                      child: Text("Submit"),
+                    ),
+                    // if(result?.data!=null)
+                    // SnackBar(content: Text(result.toString()))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -150,4 +159,27 @@ class TodoFetch {
     }
   }
 }""";
+  static String addData = r"""mutation inserData($Name:bpchar!,$Code:bpchar!){
+  insert_Countries(objects:[{Name:$Name, Code:$Code}]){
+    returning{
+      Name
+      Code
+    }
+  }
+}""";
 }
+
+String str = '''query getData{
+  Countries {
+    Code
+    Name
+  }
+}
+''';
+
+String subs = '''subscription MyQuery{
+  Countries {
+    Code
+    Name
+  }
+}''';
